f6e710dee3f3b223915f732ef5f0986521a6380a
hnnrobipozar <adamhyzy2001@wp.pl>
2024-06-18 16:56:14 +0200

    BREAKING CHANGE: added Michałek to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,14 @@ import Navigation from "./Navigation";
 class Header extends React.Component {
   render() {
     return (
-      <header className="top">
+      <header className="top" style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center", 
+      }}>
         <Navigation />
-        <h1>HOT or COLD</h1>
+        <h1>Super hot</h1>
+        <img src="/michalek.png" />
       </header>
     );
   }
